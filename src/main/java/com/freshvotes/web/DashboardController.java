808edfb084aa6dd4b1c295aaae7c71157edfe40a
Shekel-JF <kuba.flak@yahoo.com>
2023-12-03 20:06:06 +0100

    Created dashboard with logout button

--- src/main/java/com/freshvotes/web/DashboardController.java
+++ src/main/java/com/freshvotes/web/DashboardController.java
@@ -11,4 +11,10 @@ public class DashboardController
     {
         return "index";
     }
+
+    @GetMapping("/dashboard")
+    public String dashboard()
+    {
+        return "dashboard";
+    }
 }
