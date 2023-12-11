3f31191fa37e099352337af4105f0cc79c4ba57e
Shekel-JF <kuba.flak@yahoo.com>
2023-12-11 18:22:20 +0100

    Finished registration page implementation

--- src/main/java/com/freshvotes/domain/User.java
+++ src/main/java/com/freshvotes/domain/User.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import com.freshvotes.security.Authority;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -34,6 +35,7 @@ public class User
         this.id = id;
     }
 
+    @Column(unique=true)
     public String getUsername()
     {
         return username;
