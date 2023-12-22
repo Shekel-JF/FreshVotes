6be5bd999c1b8d4e2d127b008e913b199f26e15e
Shekel-JF <kuba.flak@yahoo.com>
2023-12-22 22:35:37 +0100

    Created product page template and the controller

---
+++ src/main/java/com/freshvotes/web/ProductController.java
@@ -0,0 +1,26 @@
+package com.freshvotes.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller
+public class ProductController
+{
+    @GetMapping("/products")
+    public String getProducts(ModelMap model)
+    {
+        return "product";
+    }
+    
+    @PostMapping("/products")
+    public String createProduct()
+    {
+   
+        return "redirect:/products";
+    }
+    
+}
