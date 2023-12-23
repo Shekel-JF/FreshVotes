6d692a957d9ffcb37fd121102c92b48ac87ee6da
Shekel-JF <kuba.flak@yahoo.com>
2023-12-23 23:56:39 +0100

    Made editing product's name possible

--- src/main/java/com/freshvotes/web/ProductController.java
+++ src/main/java/com/freshvotes/web/ProductController.java
@@ -20,6 +20,8 @@ import com.freshvotes.domain.User;
 import com.freshvotes.repositories.ProductRepository;
 
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
 public class ProductController
@@ -50,7 +52,6 @@ public class ProductController
         return "product";
     }
     
-
     @PostMapping("/products")
     public String createProduct(@AuthenticationPrincipal User user)
     {
@@ -62,5 +63,13 @@ public class ProductController
 
         return "redirect:/products/" + product.getId();
     }
+
+    @PostMapping("/products/{productId}")
+    public String saveProduct(@PathVariable Long productId, Product product)
+    {
+        product = productRepo.save(product);
+        return "redirect:/products/" + product.getId();
+    }
+    
     
 }
