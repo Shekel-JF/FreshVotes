69611ea13c09388960bcf023364febcc622125b1
Shekel-JF <kuba.flak@yahoo.com>
2023-12-23 15:06:55 +0100

    Made saving a product to the database possible

--- src/main/java/com/freshvotes/web/ProductController.java
+++ src/main/java/com/freshvotes/web/ProductController.java
@@ -1,26 +1,45 @@
 package com.freshvotes.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
+import com.freshvotes.domain.Product;
+import com.freshvotes.domain.User;
+import com.freshvotes.repositories.ProductRepository;
 
 @Controller
 public class ProductController
 {
+    @Autowired
+    private ProductRepository productRepo;
+
     @GetMapping("/products")
     public String getProducts(ModelMap model)
     {
         return "product";
     }
     
+    @GetMapping("/products/{productId}")
+    public String getProduct(@PathVariable Long productId) {
+        return "product";
+    }
+    
+
     @PostMapping("/products")
-    public String createProduct()
+    public String createProduct(@AuthenticationPrincipal User user)
     {
-   
-        return "redirect:/products";
+        Product product = new Product();
+        product.setPublished(false);
+        product.setUser(user);
+
+        product = productRepo.save(product);
+
+        return "redirect:/products/" + product.getId();
     }
     
 }
