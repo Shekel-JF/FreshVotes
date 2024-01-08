5358954e3df51f357713613113ab34fc1ab13304
Shekel-JF <kuba.flak@yahoo.com>
2024-01-08 15:37:43 +0100

    Fixed the Feature Controller

--- src/main/java/com/freshvotes/web/FeatureController.java
+++ src/main/java/com/freshvotes/web/FeatureController.java
@@ -1,22 +1,33 @@
 package com.freshvotes.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.freshvotes.domain.Feature;
 import com.freshvotes.service.FeatureService;
 
-@RequestMapping("products/{productId}/features")
+
+@Controller
+@RequestMapping("/products/{productId}/features")
 public class FeatureController
 {
     @Autowired
     private FeatureService featureService;
 
-    @PostMapping("/")
+    @PostMapping("")
     public String createFeature(@PathVariable long productId)
     {
-        featureService.createFeature(productId);
+        Feature feature = featureService.createFeature(productId);
+        return "redirect:/products/" + productId + "/features/" + feature.getId();
+    }
+
+    @GetMapping("{featureId}")
+    public String getFeature(@PathVariable Long productId, @PathVariable Long featureId)
+    {
         return "feature";
     }
 }
