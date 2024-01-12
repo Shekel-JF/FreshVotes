0df785ce2e0062e8ccbf4904aa2c98c74ecdacc1
Shekel-JF <kuba.flak@yahoo.com>
2024-01-12 20:09:48 +0100

    Improved the UX with proper redirects

--- src/main/java/com/freshvotes/web/FeatureController.java
+++ src/main/java/com/freshvotes/web/FeatureController.java
@@ -1,7 +1,11 @@
 package com.freshvotes.web;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -18,6 +22,8 @@ import com.freshvotes.service.FeatureService;
 @RequestMapping("/products/{productId}/features")
 public class FeatureController
 {
+    Logger log = LoggerFactory.getLogger(FeatureController.class);
+
     @Autowired
     private FeatureService featureService;
 
@@ -44,6 +50,7 @@ public class FeatureController
     public String updateFeature(Feature feature, @PathVariable Long productId, @PathVariable Long featureId)
     {
         feature = featureService.save(feature);
-        return "redirect:/products/" + productId + "/features/" + feature.getId();
+         
+        return "redirect:/p/" + feature.getProduct().getName();
     }
 }
