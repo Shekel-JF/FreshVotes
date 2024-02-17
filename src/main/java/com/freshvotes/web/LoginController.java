d32f9e7f6c942db2a05b3a1186b7ece72c069dd3
Shekel-JF <kuba.flak@yahoo.com>
2024-02-17 18:48:40 +0100

    Registration page fields are now all required, as well as confirming the password
    Also, after 10 000 years, I am finally uploading pom.xml file, which I have been forgetting for the past 3 months *oops*

--- src/main/java/com/freshvotes/web/LoginController.java
+++ src/main/java/com/freshvotes/web/LoginController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.freshvotes.domain.User;
 import com.freshvotes.service.UserService;
 
-
-
 @Controller
 public class LoginController
 {
@@ -34,7 +32,6 @@ public class LoginController
     public String registerPost(User user)
     {
         userService.save(user);
-        return "redirect:/register";
-    }
-    
+        return "redirect:/login";
+    } 
 }
